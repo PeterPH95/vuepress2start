78c147fcadbf4e63d52495678b89c7ec4f2129df
PeterPH95 <1269894219@qq.com>
2022-11-23 18:59:51 +0800

    feat:修复pwa,第4次

--- docs/.vuepress/config/pluginConf.js
+++ docs/.vuepress/config/pluginConf.js
@@ -10,16 +10,20 @@ export default [
 	searchPlugin(),
 	// PWA
 	pwaPlugin({
-		skipWaiting: false,
+		serviceWorker: true,
+		updatePopup: {
+			message: "有新东西！",
+			buttonText: "刷新"
+		}
 	}),
-	pwaPopupPlugin({
-		locales: {
-			'/': {
-				message: '有新东西！',
-				buttonText: '刷新',
-			}
-		},
-	}),
+	// pwaPopupPlugin({
+	// 	locales: {
+	// 		'/': {
+	// 			message: '有新东西！',
+	// 			buttonText: '刷新',
+	// 		}
+	// 	},
+	// }),
 	// // 看板娘设置 目前不兼容vuepress2
 	// live2d({
 	// 	// 是否开启控制台日志打印(default: false)
